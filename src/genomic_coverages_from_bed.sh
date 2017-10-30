f5f24329084ebd2298fb486cb9eea1cd54b145c1
Amir Marcovitz <amarcovitz@gmail.com>
2017-10-30 12:12:37 -0700

    src/genomic_coverages_from_bed.sh edits

--- src/genomic_coverages_from_bed.sh
+++ src/genomic_coverages_from_bed.sh
@@ -6,6 +6,9 @@
 
 # NOTE THAT: script is defaulted to hg38 as the reference genome
 
+feature_seperation=2		# seperation threshold (distance in bp) for merging nearby segments
+size_cut=10			# throw regions smaller than this size
+
 if [[ "$#" -ne 2 ]]; then
         echo -e "\nOutputs genomic-regions covered by at least the user \
 		specified amount of fragments in bed\n  Usage:\n\t$0 bed_file_list cov_thres\n"
@@ -34,7 +37,16 @@ bedtools genomecov -bga -split -i all_fragments.bed -g chrom.sizes.valid > all_f
 awk -v cov_min=${cov_thres} -F'\t' '{if($4>=cov_min) print $1"\t"$2"\t"$3"\t"NR"_"$4}' \
 	all_fragments.cov.bed > alignable_fragments.${cov_thres}_species
 
-rm -rf all_fragments.*
+# merge close enough region
+echo -e "\n\t...merging nearby (max-sep=${feature_seperation} bp) alignable fragments"
+bedtools merge -d ${feature_seperation} -i alignable_fragments.${cov_thres}_species > tmp.bed
+
+# remove small fragments
+echo -e "\n\t...removing too small (min_size=${size_cut} bp) alignable fragments"
+awk -v size_cut=${size_cut} -F'\t' '{if($3-$2>=size_cut) print $0}' \
+	tmp.bed > alignable_fragments.${cov_thres}_species
+
+rm -rf all_fragments.* tmp*
 
 echo -e "\n\t...Done!\n"
 
